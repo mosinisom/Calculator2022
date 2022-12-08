eff5b9a2ca6bd105c6e75f81cec9f3470c68da43
Alex <alexmosin@inbox.ru>
2022-12-08 18:47:56 +0500

    Сделал логику функции Press

--- Logic/Calculator.cs
+++ Logic/Calculator.cs
@@ -43,6 +43,205 @@ public class Calculator
             _screen = "0";
             _state = INPUT1;
         }
-        //...
+        else if (key == BackSpace)
+        {
+            if (_screen.Length > 1)
+                _screen = _screen.Substring(0, _screen.Length - 1);
+            else
+                _screen = "0";
+        }
+        else if (key == "=")
+        {
+            if (_state == INPUT2)
+            {
+                _screen = Calculate(_memory, _op, _screen);
+                _state = RESULT;
+            }
+        }
+        else if (key == "+/-")
+        {
+            if (_state == OPERATION)
+            {
+               // ничего не делаем
+            }
+            else if (_state == INPUT2 || _state == RESULT || _state == INPUT1)
+            {
+                if (_screen != "0")
+                {
+                    if (_screen[0] == '-')
+                        _screen = _screen[1..];
+                    else
+                        _screen = "-" + _screen;
+                }
+            }
+        }
+        else if (key == ".")
+        {
+            if (_state == RESULT)
+            {
+                _screen = "0.";
+                _state = INPUT1;
+            }
+            else if (_state == OPERATION)
+            {
+                _screen = "0.";
+                _state = INPUT2;
+            }
+            else if (_state == INPUT1 || _state == INPUT2)
+            {
+                if (!_screen.Contains('.'))
+                    _screen += ".";
+            }
+        }
+        else if (key == "+")
+        {
+            if (_state == INPUT1)
+            {
+                _memory = _screen;
+                _op = "+";
+                _state = OPERATION;
+            }
+            else if (_state == INPUT2)
+            {
+                _screen = Calculate(_memory, _op, _screen);
+                _memory = _screen;
+                _op = "+";
+                _state = OPERATION;
+            }
+            else if (_state == OPERATION)
+            {
+                _op = "+";
+            }
+            else if (_state == RESULT)
+            {
+                _memory = _screen;
+                _op = "+";
+                _state = OPERATION;
+            }
+        }
+        else if (key == "-")
+        {
+            if (_state == INPUT1)
+            {
+                _memory = _screen;
+                _op = "-";
+                _state = OPERATION;
+            }
+            else if (_state == INPUT2)
+            {
+                _screen = Calculate(_memory, _op, _screen);
+                _memory = _screen;
+                _op = "-";
+                _state = OPERATION;
+            }
+            else if (_state == OPERATION)
+            {
+                _op = "-";
+            }
+            else if (_state == RESULT)
+            {
+                _memory = _screen;
+                _op = "-";
+                _state = OPERATION;
+            }
+        }
+        else if (key == "*")
+        {
+            if(_state == INPUT1)
+            {
+                _memory = _screen;
+                _op = "*";
+                _state = OPERATION;
+            }
+            else if (_state == INPUT2)
+            {
+                _screen = Calculate(_memory, _op, _screen);
+                _memory = _screen;
+                _op = "*";
+                _state = OPERATION;
+            }
+            else if (_state == OPERATION)
+            {
+                _op = "*";
+            }
+            else if (_state == RESULT)
+            {
+                _memory = _screen;
+                _op = "*";
+                _state = OPERATION;
+            }
+        }
+        else if (key == "/")
+        {
+            if (_state == INPUT1)
+            {
+                _memory = _screen;
+                _op = "/";
+                _state = OPERATION;
+            }
+            else if (_state == INPUT2)
+            {
+                _screen = Calculate(_memory, _op, _screen);
+                _memory = _screen;
+                _op = "/";
+                _state = OPERATION;
+            }
+            else if (_state == OPERATION)
+            {
+                _op = "/";
+            }
+            else if (_state == RESULT)
+            {
+                _memory = _screen;
+                _op = "/";
+                _state = OPERATION;
+            }
+        }
+        else
+        {
+            if (_state == OPERATION)
+            {
+                _screen = key;   
+                _state = INPUT2;
+            }
+            if (_state == RESULT)
+            {
+                _screen = key;
+                _state = INPUT1;
+            }
+            if (_screen == "0")
+                _screen = key;
+            else
+                _screen += key;
+        }
+    
+    string Calculate(string mem, string op, string scr)
+    {
+        double m = double.Parse(mem);
+        double s = double.Parse(scr);
+        double r = 0;
+        switch(op)
+        {
+            case "+":
+                r = m + s;
+                break;
+            case "-":
+                r = m - s;
+                break;
+            case "*":
+                r = m * s;
+                break;
+            case "/":
+                if (s == 0)
+                {
+                    _state = ERROR;
+                    return "Error";
+                }
+                r = m / s;
+                break;
+        }
+        return r.ToString();
+    }
     }
 }
+
